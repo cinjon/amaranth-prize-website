6e0f0659eea109bd905ca1188ceb922138ce939a
Jordan Leigh <AlwaysBCoding@gmail.com>
2022-11-15 15:48:56 -0500

    adds secondary footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,6 +10,7 @@ export const Footer = () => {
 
   return (
     <div className="Footer">
+
       <div className="section section-primary">
         <div className="branding">
           <img src={branding} className="logo" alt="logo" />
@@ -29,25 +30,33 @@ export const Footer = () => {
           <div className="content-section social">
             <p className="content-title">SOCIAL</p>
             <div className="social-links">
-              <a href="https://twitter.com" target="_blank" className="social-link">
+              <a href="https://twitter.com" target="_blank" rel="noreferrer" className="social-link">
                 <img src={twitter} alt="twitter"/>
               </a>
-              <a href="https://linkedin.com" target="_blank" className="social-link">
+              <a href="https://linkedin.com" target="_blank" rel="noreferrer" className="social-link">
                 <img src={linkedin} alt="linkedin"/>
               </a>
-              <a href="https://facebook.com" target="_blank" className="social-link">
+              <a href="https://facebook.com" target="_blank" rel="noreferrer" className="social-link">
                 <img src={facebook} alt="facebook"/>
               </a>
-              <a href="https://instagram.com" target="_blank" className="social-link">
+              <a href="https://instagram.com" target="_blank" rel="noreferrer" className="social-link">
                 <img src={instagram} alt="instagram"/>
               </a>
             </div>
           </div>
         </div>
       </div>
-      <div className="section section-secondary">
 
+      <div className="section section-secondary">
+        <p className="copyright">© 2022, AMARANTH PRIZE. ALL RIGHTS RESERVED</p>
+        <div className="flex-spacer" />
+        <div className="navigation-links">
+          <Link to={"/terms-of-service"} className="navigation-link">TERMS OF SERVICE</Link>
+          <p className="vertical-bar">|</p>
+          <Link to={"/privacy-policy"} className="navigation-link">PRIVACY POLICY</Link>
+        </div>
       </div>
+
     </div>
   )
 
