39507f178bd7e0981b33d33050dabbd3a146ce53
Jordan Leigh <AlwaysBCoding@gmail.com>
2023-01-20 17:48:42 -0500

    adds toggle filter fields

--- src/screens/WinningPapersScreen.jsx
+++ src/screens/WinningPapersScreen.jsx
@@ -2,11 +2,16 @@ import React, { useState, useEffect } from "react";
 import yaml from "js-yaml";
 import tournamentbracket from "../assets/tournament-bracket.png";
 import externallink from "../assets/external-link.png";
+import checkmark from "../assets/checkmark.png";
+import useForceUpdate from "use-force-update";
 
 import papersData from "../data/papers.yml";
+import { PAPER_TAGS } from "../constants";
 
 export const WinningPapersScreen = () => {
+  const forceUpdate = useForceUpdate();
   const [papers, setPapers] = useState([]);
+  const [selectedPaperTags, setSelectedPaperTags] = useState([]);
 
   useEffect(() => {
     fetch(papersData)
@@ -16,7 +21,35 @@ export const WinningPapersScreen = () => {
       });
   }, []);
 
-  const displayPaper = (paperData, index) => {
+  const toggleFilter = (paperTag) => {
+    if(!selectedPaperTags.includes(paperTag.ident)) {
+      const nextSelectedPaperTags = selectedPaperTags;
+      nextSelectedPaperTags.push(paperTag.ident);
+      setSelectedPaperTags(nextSelectedPaperTags);
+    } else {
+      const nextSelectedPaperTags = selectedPaperTags.filter((paperIdent) => { return paperIdent !== paperTag.ident });
+      setSelectedPaperTags(nextSelectedPaperTags);
+    }
+    forceUpdate();
+  }
+
+  const renderPaperFilter = (paperTag, index) => {
+    const isChecked = selectedPaperTags.includes(paperTag.ident) ? true : false;
+    const checkboxClassName = isChecked ? "checkbox checked" : "checkbox";
+
+    return (
+      <div className="filter-item" key={`filter-item-${index}`} onClick={() => { toggleFilter(paperTag) }}>
+        <div className="checkbox-wrapper">
+          <div className={checkboxClassName}>
+            {isChecked ? <img src={checkmark} alt="checkmark" className="checkmark" /> : (<></>)}
+          </div>
+          <p className="label">{paperTag.display}</p>
+        </div>
+      </div>
+    );
+  }
+
+  const renderPaper = (paperData, index) => {
     const paperThumbnail = `${process.env.PUBLIC_URL}/paper-thumbnails/${paperData.thumbnailName}`;
 
     return (
@@ -53,9 +86,26 @@ export const WinningPapersScreen = () => {
         <img src={tournamentbracket} alt="research logo" className="section-image" />
       </div>
       <div className="section all-papers-section">
+        <div className="filters-select">
+          <div className="filters-column column-1">
+            {PAPER_TAGS.slice(0, 14).map((paperTag, index) => {
+              return renderPaperFilter(paperTag, index);
+            })}
+          </div>
+          <div className="filters-column column-2">
+            {PAPER_TAGS.slice(14, 28).map((paperTag, index) => {
+              return renderPaperFilter(paperTag, index);
+            })}
+          </div>
+          <div className="filters-column column-3">
+            {PAPER_TAGS.slice(28, 42).map((paperTag, index) => {
+              return renderPaperFilter(paperTag, index);
+            })}
+          </div>
+        </div>
         <div className="papers-display">
           {papers.map((paper, index) => {
-            return displayPaper(paper, index);
+            return renderPaper(paper, index);
           })}
         </div>
       </div>
