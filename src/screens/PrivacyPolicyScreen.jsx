f33e9ce48187f71ab839946e17112925262c678f
Jordan Leigh <AlwaysBCoding@gmail.com>
2023-01-20 19:33:27 -0500

    adds privacy policy screen

--- src/screens/PrivacyPolicyScreen.jsx
+++ src/screens/PrivacyPolicyScreen.jsx
@@ -4,7 +4,179 @@ export const PrivacyPolicyScreen = () => {
 
   return (
     <div className="screen privacy-policy-screen">
-      <p>Privacy Policy ... coming soon!</p>
+
+      <div className="content">
+
+        <h2 className="section-title">Amaranth Prize Privacy Policy</h2>
+        <h4 className="section-subtitle">Last Updated ㆍ NOV 15, 2022</h4>
+
+        <div className="section">
+          <p className="section-text">
+            S. T. Amaranth Prize Tech Ltd. (“Amaranth Prize” or “us”, “our”, “we”) respects your right to privacy. Your ability to make informed choices about the uses of your information is important to us. This privacy policy details how we collect, use and store information and data that we receive. If you have any comments or questions about our privacy policies, please contact us at info@Amaranth Prize.io.
+          </p>
+        </div>
+
+        <div className="section">
+          <p className="section-text">
+            As used in this privacy policy, “Personal Information” means information that can be directly associated with a specific person or entity such as a name, address, date of birth, or e-mail address.
+          </p>
+        </div>
+
+        <div className="section header-section">
+          <p className="section-header">Information We Collect</p>
+          <p className="section-text">
+            Amaranth Prize collects two types of personal data: data from persons who visit our website (the “Site”) as well as persons who voluntarily submit information via the Site (“Site Data”) and data concerning individuals or personnel associated with Amaranth Prize’s customers (the “Customer/s”) collected when Customers, their personnel, and/or their service providers use Amaranth Prize’s solution that assists in onboarding and training software developers codebase knowledge sharing (the “Solution”) (“Customer Data”). Site Data can include your name, email, address, company name and other contact information you submit. The Site will also record certain information automatically, such as your IP address, mobile device or computer, operating system and browser, and geo-location. In addition, we collect any information you provide when you contact us for any reason.
+          </p>
+        </div>
+
+        <div className="section">
+          <p className="section-text">
+            Customer Data includes data regarding how the Solution is used and accessed by the personnel or service providers of the Customer.
+          </p>
+        </div>
+
+        <div className="section">
+          <p className="section-text">
+            You are not required by law to provide us with any of the information described herein, but by using the Solution, visiting the Site and/or submitting any information to us you agree to provide us with such information.
+          </p>
+        </div>
+
+        <div className="section header-section">
+          <p className="section-header">How We Use Information</p>
+          <p className="section-text">
+            We will not share your Personal Information with third parties without your explicit permission, except when required by law, regulation, subpoena or court order or as otherwise expressly set forth herein. We may use Personal Information and other information internally – for example, to provide you with goods and services, to help diagnose problems with our servers, and to make our Products more useful for you and for our other customers.
+          </p>
+        </div>
+
+        <div className="section">
+          <p className="section-text">
+            We may use Personal Information to provide you with the Solution and related services. Customer Data will be shared only with the applicable Amaranth Prize Customer.
+          </p>
+        </div>
+
+        <div className="section">
+          <p className="section-text">
+            We may share your Personal Information with:
+          </p>
+        </div>
+
+        <div className="section list-section">
+          <p className="list-icon">•</p>
+          <p className="section-text">
+            Members of our corporate family, or third party vendors or service providers, such as email service providers sending emails on our behalf. We may also authorize third party vendors or service providers to collect information on our behalf. We will require members of our corporate family and third party vendors to follow practices that protect your privacy rights on a level at least as restrictive as the practices described in this Privacy Policy.
+          </p>
+        </div>
+
+        <div className="section list-section">
+          <p className="list-icon">•</p>
+          <p className="section-text">
+            Business partners with whom we may jointly offer products or services, or whose products or services may be offered on our website. You can tell when a third party is involved in a product or service because their name will appear, either alone or with ours. If you choose to use these products or services, we may share information about you, including your Personal Information, with those partners. Please note that we do not control the privacy practices of these third party business partners. If you wish to no longer allow that third party to use your information, please contact that third party directly.
+          </p>
+        </div>
+
+        <div className="section list-section">
+          <p className="list-icon">•</p>
+          <p className="section-text">
+            Referring websites. If you were referred to this website from another site (for example, through a link), we may share some information about you with that referring website. We encourage you to review the privacy policies of any website that referred you here. Additionally, when you link to one of our partners from our Services, we provide that partner with a unique identifier so they know that you were referred from our Services. This unique identifier will not contain any of your Personal Information.
+          </p>
+        </div>
+
+        <div className="section">
+          <p className="section-text">
+           In addition, by analyzing all information we receive, we may compile statistical information across a variety of users (<span className="emphasis">“Statistical Information”</span>). Statistical Information helps understand trends and customer needs so that new products and services can be considered and so existing products and services can be tailored to customer desires. Statistical Information does not indicate individuals’ identities, nor customers’ identities, and we will not link Statistical Information to any Personal Information. We may share Statistical Information with our partners, pursuant to commercial terms that we determine in our sole discretion. In addition, we may provide any information to third parties, so long as we have removed any information that identifies any particular individual or user, such as a name, address or contact information. We also use a tool called “Google Analytics” to collect information about the use of the Solution. Google Analytics collects information such as how often users visit the Solution, what pages they visit when they do so, and what other sites they used prior to coming to the Solution. Google Analytics collects only the IP address assigned to you on the date you visit the Solution, rather than your name or other identifying information. We do not combine the information collected through the use of Google Analytics with personally identifiable information. We use the information we get from Google Analytics only to improve our Solution. Google’s ability to use and share information collected by Google Analytics about your visits to the Solution is restricted by the Google Analytics Terms of Use located at http://www.google.com/analytics/terms/us.html and the Google Privacy Policy located at http://www.google.com/policies/privacy/.
+          </p>
+        </div>
+
+        <div className="section header-section">
+          <p className="section-header">How We Protect Information</p>
+          <p className="section-text">
+            We take great care in implementing and maintaining the security and follow generally accepted industry standards to protect the Personal Information submitted to us, both during transmission and once we receive it. However, no method of transmission over the Internet, or method of electronic storage is 100% secure. Therefore, while we strive to use commercially acceptable means to protect your personal information, we cannot guarantee its absolute security.
+          </p>
+        </div>
+
+        <div className="section">
+          <p className="section-text">
+            We will delete any information provided to us by a user upon the receipt of a written request by such user. We cannot restore information once it has been deleted.
+          </p>
+        </div>
+
+        <div className="section header-section">
+          <p className="section-header">Cookies</p>
+          <p className="section-text">
+            A cookie is a small piece of text that is sent to a visitor's browser. The browser provides this piece of text to the device of the originating visitor when this visitor returns. We use “persistent” cookies which may be used to help save your settings and customizations across visits and which are saved on your device for a fixed period of time and are not deleted when the browser is closed. In addition, we use session cookies which are stored only temporarily during a browsing session in order to allow normal use of the Solution and are revoked after one month. We also use third-party cookies which are set by other online services that run content on the page you view on your browser. Most Web browsers are initially configured to accept cookies, but you can change this setting so your browser either refuses all cookies or informs you when a cookie is being sent. Also, you are free to delete any existing cookies at any time. Please note that some features of the Solution may not function properly when cookies are disabled or removed.
+          </p>
+        </div>
+
+        <div className="section header-section">
+          <p className="section-header">Third Party Sites and Services</p>
+          <p className="section-text">
+            This Privacy Policy only addresses the use and disclosure of information we collect from you. To the extent that you disclose any information, including Personal Information, to other parties through the Services, for example by way of clicking on a link to any other website or location or via other sites throughout the Internet, different terms may apply to the use or disclosure of the information you disclose to such third parties. You acknowledge that Amaranth Prize is not responsible for privacy practices such third parties, and that this Privacy Policy does not apply to such third parties. You are knowingly and voluntarily assuming all risks of using third-party sites to purchase products and services. You agree that Amaranth Prize shall have no liability whatsoever with respect to such third-party sites and your usage of them.
+          </p>
+        </div>
+
+        <div className="section">
+          <p className="section-text">
+            We use third party providers to provide services on the Solution, however we do not transfer any parties any Personal Data. We are not affiliated with, and do not have any responsibility for the privacy practices of such third party payment services. Please review independently the privacy policies of such third party payment providers.
+          </p>
+        </div>
+
+        <div className="section header-section">
+          <p className="section-header">Children</p>
+          <p className="section-text">
+            We do not knowingly collect Personal Information from children under the age of 13. In the event you become aware that an individual under the age of 13 has enrolled without parental permission, please advise us immediately.
+          </p>
+        </div>
+
+        <div className="section header-section">
+          <p className="section-header">Correcting or Updating Your Information</p>
+          <p className="section-text">E-mail address: info@Amaranth Prize.io</p>
+          <p className="section-text">Postal Address: 8, Haarbaa St., Tel Aviv, Israel.</p>
+        </div>
+
+        <div className="section">
+          <p className="section-text">
+            We will retain your information as needed to provide you services. If you wish to request that we no longer use or keep your information, please contact us at the e-mail address or postal address listed above. We will retain and use your information as necessary to comply with our legal obligations, resolve disputes, and enforce our agreements.
+          </p>
+        </div>
+
+        <div className="section header-section">
+          <p className="section-header">Communications</p>
+          <p className="section-text">
+            We may send you email or other messages about us or our products and services as well as messages about products and services of selected third parties. By accepting the Terms for the Solution (including the terms of this Privacy Policy which are incorporated therein) or using the Solution, you affirmatively consent to receive such commercial messages. As a registered member, you can remove your personal information from our mailing list and to stop receiving future communication from us by following the unsubscribe link located at the bottom of each communication or by emailing us at info@Amaranth Prize.io. You will also be given the opportunity to unsubscribe from commercial messages in any such email we send. Please note that we reserve the right to send you service related communications, including service announcements and administrative messages, relating either to your account or to your transactions on the Solution, without offering you the opportunity to opt out of receiving them unless you cancel your account.
+          </p>
+        </div>
+
+        <div className="section header-section">
+          <p className="section-header">Other Uses or Transfer of Your Information</p>
+          <p className="section-text">
+            We will fully cooperate with any law enforcement authorities or court order requesting or directing us to disclose the identity or behavior of any user suspected to have engaged in illegal behavior.
+          </p>
+        </div>
+
+        <div className="section">
+          <p className="section-text">
+            We may use subcontractors that are located in countries other than your own, and send them information we receive. For example, we may send your information to any countries outside your jurisdiction and/or outside the United States or the European Union. All our subcontractors will be subject to non-disclosure and non-use obligations. Nevertheless, some countries may have levels of protection of personal and other information which are not as high as in your country of residence or business.
+          </p>
+        </div>
+
+        <div className="section">
+          <p className="section-text">
+            We may transfer our databases containing your Personal Information if we sell our business or part of it , including in cases of liquidation, and the terms of this Privacy Policy will continue to apply to the databases. Information about our users, including Personal Information, may be disclosed as part of, or during negotiations of, any merger, sale of company assets or acquisition and shall continue being subject to the provisions of this Privacy Policy. By providing such information, you expressly consent to such transfer and use, including transfers outside of the country of your residence.
+          </p>
+        </div>
+
+        <div className="section header-section">
+          <p className="section-header">Changes</p>
+          <p className="section-text">We may update this privacy policy from time to time, and we encourage you to review it periodically.</p>
+        </div>
+
+        <div className="section header-section">
+          <p className="section-header">Comments and Questions</p>
+          <p className="section-text">If you have any comments or questions about our privacy policy, please contact us at info@Amaranth Prize.io.</p>
+        </div>
+
+      </div>
+
     </div>
   )
 
