0cdde1dcbfe23ff78f48cc4c0a458cec9876d2f5
Cinjon Resnick <cinjon.resnick@gmail.com>
2023-01-22 18:53:38 -0500

    About screen update.

--- src/screens/AboutScreen.jsx
+++ src/screens/AboutScreen.jsx
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from "react";
+import React, { useRef, useState, useEffect } from "react";
+import {useLocation} from "react-router-dom";
 import yaml from "js-yaml";
 
 import voterfraud from "../assets/voter-fraud.png";
@@ -8,6 +9,11 @@ import expertsData from "../data/experts.yml";
 
 export const AboutScreen = () => {
   const [experts, setExperts] = React.useState([]);
+    const { hash } = useLocation();
+
+    const selectionProcessRef = useRef(null)
+    const expertsSectionRef = useRef(null)
+    const awardingSectionRef = useRef(null)
 
   useEffect(() => {
     fetch(expertsData)
@@ -17,13 +23,35 @@ export const AboutScreen = () => {
       });
   }, []);
 
+    useEffect(() => {
+        if (hash === "#selection-process") {
+            selectionProcessRef.current.scrollIntoView({
+                behavior: 'smooth',
+                block: 'start',
+                inline: 'center',
+            });
+        } else if (hash === "#experts-section") {
+            expertsSectionRef.current.scrollIntoView({
+                behavior: 'smooth',
+                block: 'start',
+                inline: 'center',
+            });            
+        } else if (hash === "#awarding-section") {
+            awardingSectionRef.current.scrollIntoView({
+                behavior: 'smooth',
+                block: 'start',
+                inline: 'center',
+            });
+        }
+    }, [])
+
   const renderExpert = (expertData) => {
     return (
       <div className="expert">
         <p className="expert-title">{expertData.title}</p>
         <p className="expert-bio">{expertData.bio}</p>
-        <a className="expert-linkedin" href={expertData.linkedin ?? "#"} target="_blank" rel="noreferrer">
-          <p className="linkedin-text">Linkedin</p>
+        <a className="expert-linkedin" href={expertData.link ?? "#"} target="_blank" rel="noreferrer">
+          <p className="linkedin-text">More</p>
           <img src={rightArrow} alt="right-arrow" className="view-more-arrow" />
         </a>
       </div>
@@ -36,41 +64,42 @@ export const AboutScreen = () => {
       <div className="section banner-section">
         <div className="text-content">
           <p className="primary">We believe in promoting <span className="emphasis">healthier and longer lifespan</span> for all humans.</p>
-          <p className="secondary">The Amaranth Prize rewards people who spend their precious time pushing this field. Our retrospective approach gives researchers the freedom to pursue important work and reward the research that influence them.</p>
+          <p className="secondary">The Amaranth Prize rewards scientists who spend their precious time pushing this field forward. Our retrospective approach gives researchers the freedom to pursue important work and reward the research that influence them.</p>
         </div>
         <div className="flex-spacer" />
         <img src={voterfraud} alt="lifespan" className="section-image" />
       </div>
 
-      <div className="section selection-process-section">
+      <div className="section selection-process-section" ref={selectionProcessRef} id="selection-process">
         <p className="section-title">SELECTION PROCESS</p>
         <p className="section-description">We choose a topic within the field and convene a panel of experts to select and award outstanding work published in recent years.</p>
         <div className="selection-process-steps">
           <div className="selection-process-step">
             <p className="step-number">1</p>
             <p className="step-title">Expert selection</p>
-            <p className="step-details">Once we had selected the topic - “Brain aging exclusive of Alzheimer's disease" - we then convened a panel of 100 experts from this field to choose which contributions were most deserving.</p>
+            <p className="step-details">Once we had selected the topic - “Protein Aging 2015 - 2021" - we then convened a panel of eight experts from this field to choose which contributions were most deserving.</p>
           </div>
           <div className="selection-process-step">
             <p className="step-number">2</p>
-            <p className="step-title">Expanding the queries</p>
-            <p className="step-details">Beyond the contributions they knew from memory, we added works that fit the following query criteria on Semantic Scholar and Google Scholar: Query a, Query b, and Query c.</p>
+            <p className="step-title">Creating a shortlist</p>
+            <p className="step-details">We first filtered a catalog of more than 2800 papers gleaned from Semantic Scholar. These were the union of results from a diverse set of queries (available on demand). In the spirit of scientists knowing what works they admire, we also had them contribute papers that they considered highly.
+            </p>
           </div>
           <div className="selection-process-step">
             <p className="step-number">3</p>
-            <p className="step-title">Filtering the pool</p>
-            <p className="step-details">This yielded a total of 1000 contributions. We convened a smaller panel to pick the final selections. Their instructions were to focus on results or impact, reward the prize non-uniformly, and lean into both unanimous and polarizing works. From this process, we found our 30 contributions.</p>
+            <p className="step-title">Reducing the shortlist</p>
+            <p className="step-details">This shortlist was 181 papers. We then filtered it to be more exactly in the field of Protein Aging and removed contributions outside of our target time range for a total of 42 papers. From these, we reviewed the expert rankings and found there were clear tiers. The 15 winners represent the top tier.</p>
           </div>
         </div>
       </div>
 
-      <div className="section awarding-the-prize-section">
-        <p className="section-title">AWARDING THE PRIZE</p>
+      <div className="section awarding-the-prize-section" ref={awardingSectionRef} id="awarding-section">
+       <p className="section-title">AWARDING THE PRIZE</p>
         <p className="section-description">We leverage the <span className="emphasis">Research Portfolio</span> platform to distribute awards, creating a simple, on-chain record of prizes and influences.</p>
         <div className="awarding-infos">
           <div className="awarding-info">
             <p className="info-title">Claiming process</p>
-            <p className="info-details">Researchers will mint their research using their unique technology on Research Portfolio. Afterwards they can claim their prize by simply connecting their wallets to Amaranth Prize website.</p>
+            <p className="info-details">Researchers mint their research using unique technology on Research Portfolio. Afterwards they can claim their prize by simply connecting their wallets to the Amaranth Prize website.</p>
           </div>
           <div className="awarding-info">
             <p className="info-title">Influencing Papers</p>
@@ -79,7 +108,7 @@ export const AboutScreen = () => {
         </div>
       </div>
 
-      <div className="section experts-section">
+      <div className="section experts-section" ref={expertsSectionRef} id="experts-section">
         <p className="section-title">THE EXPERTS</p>
         <p className="section-description">This is a list of our generous experts giving their time to choose the most impactful research in longevity this year.</p>
         <div className="experts">
@@ -100,7 +129,6 @@ export const AboutScreen = () => {
             {experts[7] ? renderExpert(experts[7]) : (<></>)}
           </div>
         </div>
-        <p className="more-experts-disclaimer">More experts will be added to the panel</p>
       </div>
     </div>
   )
